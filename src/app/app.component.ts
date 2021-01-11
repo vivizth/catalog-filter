d3304ab2a1e5be7ecef68f7e75272bb9a33b43f8
oak <vivizth@gmail.com>
2021-01-11 14:17:55 +0700

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { combineLatest, Observable } from 'rxjs';
+import { filter, map, startWith, tap, withLatestFrom } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'category';
+  api: 'https://api.publicapis.org/categories';
+  allList$: Observable<string[]>;
+  filter$: Observable<string[]>;
+  form: FormGroup;
+
+  constructor(private http: HttpClient, private fb: FormBuilder){
+    this.form = fb.group({
+      query: ['']
+    })
+    this.allList$ = this.http.get<string[]>('https://api.publicapis.org/categories');
+    const query = this.form.get('query').valueChanges.pipe(startWith(''));
+    this.filter$ = combineLatest([this.allList$, query]).pipe(
+      map(this.doFilter)
+    )
+  }
+
+  doFilter = ([list, query]) => {
+    query = query.toLowerCase();
+    return list.filter(l => { 
+      l = l.toLowerCase();
+      return l.indexOf(query) > -1;
+    })
+  }
+  
 }
